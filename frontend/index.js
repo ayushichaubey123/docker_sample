ac9c5188dbca73b86c67fb107d45d279c2716676
ayushichaubey123 <ayushichaubey911@gmail.com>
2024-04-25 14:14:27 +0530

    n

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,3 @@
 const fecthData = async()=>{
-    const data = await fetch(" ./index.html ");
+    const data = await fetch(" https://662914f7c8162c2a28eefe5e--curious-daffodil-9df457.netlify.app/ ");
 }
